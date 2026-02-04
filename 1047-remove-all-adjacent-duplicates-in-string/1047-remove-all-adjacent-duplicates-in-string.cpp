9a4848e8882c5f1ab843cafa781dd1688d1e4801
Karan singh <127647065+karan49348@users.noreply.github.com>
2026-02-04 11:15:39 +0530

    Time: 11 ms (28.64%), Space: 14.2 MB (42.22%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
+        stack<char> st;
         string ans = "";
 
-        for (char ch : s) {
-            if (!ans.empty() && ans.back() == ch) {
-                ans.pop_back();   
-            } else {
-                ans.push_back(ch); 
+        for(auto x : s){
+            if(st.empty()){
+                st.push(x);
+            }
+            else if(x == st.top()){
+                st.pop();
+            }
+            else{
+                st.push(x);
             }
         }
+
+        while(!st.empty()){
+            ans += st.top();  
+            st.pop();          
+        }
+
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
