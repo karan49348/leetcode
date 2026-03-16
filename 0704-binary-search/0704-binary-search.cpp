e46398d0cc4e4aded6adaf5f6c8872332f802aa7
Karan singh <127647065+karan49348@users.noreply.github.com>
2026-03-16 11:43:18 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.68%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,16 +4,18 @@ public:
         int left=0;
         int right=nums.size()-1;
         while(left<=right){
-            int mid=(left+right)/2;
-
+            int mid= left +(right-left)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<target){
-                left=mid+1;
-            }else if(nums[mid]>target){
-                right=mid-1;
             }
+                else if(nums[mid]<target){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            
         }
         return -1;
+        
     }
 };
